adf11439dc3bba40fa5db98c5b0c96f92b459f4f
818macpro <a818@818macprodeMacBook-Pro.local>
2014-12-17 09:56:21 +0800

    Initial Commit

---
+++ iDrug818/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDrug818
+//
+//  Created by 818macpro on 14-12-17.
+//  Copyright (c) 2014年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iDrug818/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iDrug818
+//
+//  Created by 818macpro on 14-12-17.
+//  Copyright (c) 2014年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
